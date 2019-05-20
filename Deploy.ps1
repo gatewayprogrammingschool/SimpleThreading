fc65d40dc671a6c76e02586d509066271d75f28a
The Sharp Ninja <ninja@thesharp.ninja>
2019-05-20 12:47:41 -0500

    Migrated deploy script from Paket to Nuget.

--- Deploy.ps1
+++ Deploy.ps1
@@ -7,31 +7,32 @@ function Deploy-Package
         [Parameter(Mandatory=$true)][string]$Assembly
     )	
 
-    $proj = $SolutionDir + '\' + $Namespace + '\' + $Namespace + '.csproj'
+    $projFolder = $SolutionDir + '\' + $Namespace + '\'
+    $proj = $projFolder + $Namespace + '.csproj'
 	$assm = $BuildDir + '\' + $Assembly
+	$nuspec = $SolutionDir + '\' + $Namespace + '\' + $Namespace + '.nuspec'
+    $nupkg = $SolutionDir + '\Assets\' + $Namespace + '*.nupkg'
+	$apiKey = $env:ApiKey
+	$source = $env:NugetSource
 
-&    "F:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\msbuild.exe" $proj /p:Configuration=Release
+    Set-Location $projFolder
+
+    & "C:\Program Files (x86)\Microsoft Visual Studio\2017\MSBuild\15.0\Bin\msbuild.exe" $proj /p:Configuration=Release
 
 	if($LASTEXITCODE -eq 0) {
-
-
-		$AssemblyVersion = 
-			[Diagnostics.FileVersionInfo]::GetVersionInfo($assm).FileVersion
-    
-		$packageName = $Namespace +'.' +$AssemblyVersion+'.nupkg'
-		$package = $BuildDir +'\' +$packageName
-		$destination = $BuildDir + '\' + $packageName + "\" + $packageName
-		$sourceDestination = $BuildDir + '\' + $packageName + "\" + $Namespace +'.' +$AssemblyVersion+'.symbols.nupkg'
-		Set-Location $SolutionDir
-
-		$releaseNotes = [IO.File]::ReadAllText($SolutionDir + "\ReleaseNotes.txt")
-
-		.paket/paket.exe pack --release-notes $releaseNotes --symbols -v $package
-		Paket-Push -File $destination -ApiKey $env:NugetAPIKey -url https://www.nuget.org -endpoint /api/v2/package -Verbose
+		& nuget.exe pack $nuspec -OutputDirectory ..\Assets\
+        if($LASTEXITCODE -eq 0) {
+            $package = Get-ChildItem $nupkg | Sort-Object | Select-Object -Last 1
+            & nuget.exe push $package.FullName -ApiKey $apiKey -Source $source
+        }
 	}
 }
 
 Clear-Host
 
 #Deploy-Package -SolutionDir %1 -BuildDir %2 -Namespace %3 -Assembly %4
-Deploy-Package -SolutionDir 'F:\GPS\SimpleThreading\SimpleThreading' -BuildDir 'F:\GPS\SimpleThreading\SimpleThreading\GPS.SimpleThreading\bin\Release' -Namespace "GPS.SimpleThreading" -Assembly "GPS.SimpleThreading.dll"
+Deploy-Package `
+	-SolutionDir 'C:\GitHub\SimpleThreading\SimpleThreading'`
+	-BuildDir 'C:\GitHub\SimpleThreading\SimpleThreading\GPS.SimpleThreading\bin\Release'`
+	-Namespace "GPS.SimpleThreading"`
+	-Assembly "GPS.SimpleThreading.dll"
