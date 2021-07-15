ce86eab2ebf35a936986cadce71cfcbcedbc67b1
The Sharp Ninja <ninja@thesharp.ninja>
2021-07-15 15:24:54 -0500

    Fixing Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -6,39 +6,39 @@ function Deploy-Package {
         [Parameter(Mandatory = $true)][string]$Assembly
     )	
 
-    $projFolder = $SolutionDir + '\' + $Namespace + '\'
+    $projFolder = $SolutionDir + '/' + $Namespace + '/'
     $proj = $projFolder + $Namespace + '.csproj'
-    $assm = $BuildDir + '\' + $Assembly
-    $nuspec = $SolutionDir + '\' + $Namespace + '\' + $Namespace + '.nuspec'
-    $nupkg = $SolutionDir + '\Assets\' + $Namespace + '*.nupkg'
+    $assm = $BuildDir + '/' + $Assembly
+    $nuspec = $SolutionDir + '/' + $Namespace + '/' + $Namespace + '.nuspec'
+    $nupkg = $SolutionDir + '/Assets/' + $Namespace + '*.nupkg'
     $apiKey = $env:ApiKey
     $source = $env:NugetSource
+
+    Write-Host "`$SolutionDir: $SolutionDir"
+    Write-Host "`$BuildDir: $BuildDir"
+    Write-Host "`$Namespace: $Namespace"
+    Write-Host "`$Assembly: $Assembly"
+    Write-Host "`$projFolder: $projFolder"
+    Write-Host "`$proj: $proj"
+    Write-Host "`$assm: $assm"
+    Write-Host "`$nuspec: $nuspec"
+    Write-Host "`$nupkg: $nupkg"
+    Write-Host "`$proj: $proj"
+    Write-Host "`$source: $source"
 
     if(Test-Path $projFolder) {
         Set-Location $projFolder
 
-        & dotnet build $proj --configuration Release --no-restore
+        & dotnet build --configuration Release --no-restore
 
         if ($LASTEXITCODE -eq 0) {
-            & nuget.exe pack $nuspec -OutputDirectory ..\Assets\
+            & nuget.exe pack $nuspec -OutputDirectory ../Assets/
             if ($LASTEXITCODE -eq 0) {
                 $package = Get-ChildItem $nupkg | Sort-Object | Select-Object -Last 1
                 & nuget.exe push $package.FullName -ApiKey $apiKey -Source $source
             }
         }
     } else {
-        Write-Host "`$SolutionDir: $SolutionDir"
-        Write-Host "`$BuildDir: $BuildDir"
-        Write-Host "`$Namespace: $Namespace"
-        Write-Host "`$Assembly: $Assembly"
-        Write-Host "`$projFolder: $projFolder"
-        Write-Host "`$proj: $proj"
-        Write-Host "`$assm: $assm"
-        Write-Host "`$nuspec: $nuspec"
-        Write-Host "`$nupkg: $nupkg"
-        Write-Host "`$proj: $proj"
-        Write-Host "`$source: $source"
-
         throw "Project Folder not found."
     }
 }
@@ -47,7 +47,7 @@ Clear-Host
 
 #Deploy-Package -SolutionDir %1 -BuildDir %2 -Namespace %3 -Assembly %4
 Deploy-Package `
-    -SolutionDir './'`
+    -SolutionDir './src'`
     -BuildDir './src/GPS.SimpleThreading/bin/Release'`
     -Namespace "GPS.SimpleThreading"`
     -Assembly "GPS.SimpleThreading.dll"
